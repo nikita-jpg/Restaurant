45df02246f27e9dccf1abf5da1dd2f4b3ce6de3f
Никита <nicitazaletin@yandex.ru>
2021-06-08 19:13:32 +0300

    Добавил карту на панель с информацией

--- src/comonents/About/About.js
+++ src/comonents/About/About.js
@@ -47,7 +47,7 @@ const About = () => {
             <div className="About__discounts">
                 {
                 discountInfo.map(record => (
-                    <div className="About__discount__block">
+                    <div className="About__discount-block">
                         <div className="About__logo" style={{backgroundImage: `url(${record.img})`}}></div>
                         <div className="About__text-block">
                             <span class="About__title">{record.title}</span>
@@ -57,17 +57,17 @@ const About = () => {
                 ))}
             </div>
             <div className="About__info">
-                {
+                <div className="About__info-container">{
                     restarauntInfo.map( record =>(
-                        <div className="About__info__block">
+                        <div className="About__info-block">
                             <div className="About__logo" style={{backgroundImage: `url(${record.img})`}}></div>
                             <span class="About__title">{record.title}</span>
                             <span class="About__description">{record.description}</span>
                         </div>
                     ))
-                }
+                }</div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4501.218277081453!2d37.47396850585936!3d55.661006927490206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2023687053decdb4!2z0J7QsdGJ0LXQttC40YLQuNC1IOKEljEg0JzQmNCi0KXQog!5e0!3m2!1sru!2sru!4v1623159836557!5m2!1sru!2sru" width="600" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe>
             </div>
-            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4501.218277081453!2d37.47396850585936!3d55.661006927490206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2023687053decdb4!2z0J7QsdGJ0LXQttC40YLQuNC1IOKEljEg0JzQmNCi0KXQog!5e0!3m2!1sru!2sru!4v1623159836557!5m2!1sru!2sru" width="600" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe> */}
         </div>
     )
 }
