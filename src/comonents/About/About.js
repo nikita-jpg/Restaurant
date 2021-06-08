268d37a781b671a618a328df9f1334645843e750
Никита <nicitazaletin@yandex.ru>
2021-06-09 01:17:12 +0300

    иконки

--- src/comonents/About/About.js
+++ src/comonents/About/About.js
@@ -24,22 +24,22 @@ const About = () => {
         {
             title:"АДРЕС",
             description:"Москва, \nКутузовский проспект, 2/1с6",
-            img:"./img/discount.svg"
+            img:"./img/placeholder.svg"
         },
         {
-            title:"АДРЕС",
-            description:"Москва,\nКутузовский проспект, 2/1с6",
-            img:"./img/discount.svg"
+            title:"ЧАСЫ РАБОТЫ",
+            description:"Ежедневно\n09:00 - 00:00",
+            img:"./img/clock.svg"
         },
         {
-            title:"АДРЕС",
-            description:"Москва,\nКутузовский проспект, 2/1с6",
-            img:"./img/discount.svg"
+            title:"ТЕЛЕФОН",
+            description:"+7 (495) 161 61 42",
+            img:"./img/iphone.svg"
         },
         {
-            title:"АДРЕС",
-            description:"Москва,\nКутузовский проспект, 2/1с6",
-            img:"./img/discount.svg"
+            title:"ПОЧТА",
+            description:"info@matryoshka-rest.ru",
+            img:"./img/message.svg"
     }]
 
     const socialInfo = [
@@ -66,6 +66,13 @@ const About = () => {
                 ))}
             </div>
             <div className="About__info">
+            <div className="About__contacts-container">
+                {
+                    socialInfo.map(img => (
+                        <div className="About__contact" style={{backgroundImage: `url(${img})`}}></div>
+                        ))
+                }
+                </div>
                 <div className="About__info-container">{
                     restarauntInfo.map( record =>(
                         <div className="About__info-block">
@@ -75,15 +82,6 @@ const About = () => {
                         </div>
                     ))
                 }</div>
-                <div className="About__contacts-container">
-                    {
-                        socialInfo.map(service => (
-                            <div className="About__contact">
-                                
-                            </div>
-                        ))
-                    }
-                </div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4501.218277081453!2d37.47396850585936!3d55.661006927490206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2023687053decdb4!2z0J7QsdGJ0LXQttC40YLQuNC1IOKEljEg0JzQmNCi0KXQog!5e0!3m2!1sru!2sru!4v1623159836557!5m2!1sru!2sru" width="600" height="310" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
