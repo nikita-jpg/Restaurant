015452b22ac1695e00bcdb6554c8184d705b02a7
Nikita <nicitazaletin@yandex.ru>
2022-05-16 13:17:20 +0300

    Работает отправка информации о заказе

--- src/comonents/Modal/Modal.js
+++ src/comonents/Modal/Modal.js
@@ -5,26 +5,51 @@ import "./modal.css";
 const Modal = ({ modal, setModal }) => {
   const makeRecordUri = "http://localhost:8080/booking/makeRecord?";
   const [inputName, setName] = useState("");
-  const [inputSecondName, setSecondName] = useState("");
+  const [inputAddress, setAddress] = useState("");
   const [inputTelNumber, setTelNumber] = useState("");
+  const [inputDate, setDate] = useState("");
   const [timeStart, setTimeStart] = useState("");
   const [timeEnd, setTimeEnd] = useState("");
+  const [comment, setComment] = useState("");
+
+  const http = axios.create({
+    headers: {
+      // Прикрепляем заголовок, отвечающий за параметры запуска.
+      Authorization: "12345",
+    },
+  });
 
   const sendRecord = () => {
-    console.log(modal.freeTime);
-    axios({
-      method: "get",
-      url: makeRecordUri,
-      params: {
-        deskNumber: modal.tableId,
-        date: modal.date,
-        timeStart: "14:00",
-        timeEnd: "15:00",
-        clientName: inputName,
-        clientSecondName: inputSecondName,
-        clientTelNumber: inputTelNumber,
-      },
-    });
+    http
+      .post("http://127.0.0.1:18301/" + "addZakaz", {
+        name: inputName,
+        address: inputAddress,
+        fhoneNumber: inputTelNumber,
+        date: inputDate,
+        timeStart: timeStart,
+        timeEnd: timeEnd,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    // .then((data) => {
+    //   return data.data;
+    // })
+    // .catch((data) => {
+    //   return null;
+    // });
+    // console.log(modal.freeTime);
+    // axios({
+    //   method: "post",
+    //   url: makeRecordUri,
+    //   params: {
+    //     name: inputName,
+    //     adress: inputAddress,
+    //     foneNumber: inputTelNumber,
+    //     date: inputDate,
+    //     timeStart: timeStart,
+    //     timeEnd: timeEnd,
+    //   },
+    // });
   };
 
   return (
@@ -48,8 +73,8 @@ const Modal = ({ modal, setModal }) => {
           <p className="modal__input__p">Введите ваш адрес</p>
           <input
             className="modal__input__input"
-            value={inputSecondName}
-            onChange={(event) => setSecondName(event.target.value)}
+            value={inputAddress}
+            onChange={(event) => setAddress(event.target.value)}
           ></input>
         </div>
 
@@ -59,6 +84,15 @@ const Modal = ({ modal, setModal }) => {
             className="modal__input__input"
             value={inputTelNumber}
             onChange={(event) => setTelNumber(event.target.value)}
+          ></input>
+        </div>
+
+        <div className="modal__input">
+          <p className="modal__input__p">Введите дату</p>
+          <input
+            className="modal__input__input"
+            value={inputDate}
+            onChange={(event) => setDate(event.target.value)}
           ></input>
         </div>
 
@@ -88,6 +122,15 @@ const Modal = ({ modal, setModal }) => {
                 onChange={(event) => setTimeEnd(event.target.value)}
               ></input>
             </div>
+          </div>
+
+          <div className="modal__input">
+            <p className="modal__input__p">Комментарий к заказу</p>
+            <input
+              className="modal__input__input"
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+            ></input>
           </div>
         </div>
 
